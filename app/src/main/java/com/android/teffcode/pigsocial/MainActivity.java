89209bd3f18596310e14c202f9791edad628a878
teffcode <tefy.1@live.com>
2017-05-10 23:30:57 -0500

    Contact adapter created

--- app/src/main/java/com/android/teffcode/pigsocial/MainActivity.java
+++ app/src/main/java/com/android/teffcode/pigsocial/MainActivity.java
@@ -2,11 +2,21 @@ package com.android.teffcode.pigsocial;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+
+import com.android.teffcode.pigsocial.adapter.ContactoAdaptador;
+import com.android.teffcode.pigsocial.pojo.Contacto;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
+    private RecyclerView rvContactos;
+    private ArrayList<Contacto> contactos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         toolbar = (Toolbar) findViewById(R.id.toolbar);
+        rvContactos = (RecyclerView) findViewById(R.id.rvContacto);
 
         if(toolbar != null){
             setSupportActionBar(toolbar);
@@ -21,6 +32,28 @@ public class MainActivity extends AppCompatActivity {
             getSupportActionBar().setIcon(R.drawable.pig_filled_50);
         }
 
+        //generarLinearLayoutVertical();
+        //generarGridLayout();
+
+    }
+/*
+    public void generarLinearLayoutVertical() {
+        LinearLayoutManager llm = new LinearLayoutManager(getActivity());
+        llm.setOrientation(LinearLayoutManager.VERTICAL);
+        rvContactos.setLayoutManager(llm);
     }
 
+    public void generarGridLayout() {
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(getContext(), 2);
+        rvContactos.setLayoutManager(gridLayoutManager);
+    }
+
+    public ContactoAdaptador crearAdaptador(ArrayList<Contacto> contactos) {
+        ContactoAdaptador adaptador = new ContactoAdaptador(contactos, getActivity());
+        return adaptador;
+    }
+
+    public void inicializarAdaptadorRV(ContactoAdaptador adaptador) {
+        rvContactos.setAdapter(adaptador);
+    }*/
 }
---
+++ app/src/main/java/com/android/teffcode/pigsocial/adapter/ContactoAdaptador.java
@@ -0,0 +1,67 @@
+package com.android.teffcode.pigsocial.adapter;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.android.teffcode.pigsocial.R;
+import com.android.teffcode.pigsocial.pojo.Contacto;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+/**
+ * Created by user on 10/05/17.
+ */
+
+public class ContactoAdaptador extends RecyclerView.Adapter<ContactoAdaptador.ContactoViewHolder>{
+
+    ArrayList<Contacto> contactos;
+    Activity activity;
+
+    public ContactoAdaptador(ArrayList<Contacto> contactos, Activity activity) {
+        this.contactos = contactos;
+        this.activity = activity;
+    }
+
+    @Override
+    public ContactoViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.cardview_perfil, parent, false);
+        return new ContactoViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(final ContactoViewHolder contactoViewHolder, int position) {
+        final Contacto contacto = contactos.get(position);
+        Picasso.with(activity)
+                .load(contacto.getUrlFoto())
+                .placeholder(R.drawable.button_like)
+                .into(contactoViewHolder.imgFoto);
+        contactoViewHolder.txtLikes.setText(String.valueOf(contacto.getLikes()));
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return contactos.size();
+    }
+
+    public static class ContactoViewHolder extends RecyclerView.ViewHolder {
+
+        private ImageView imgFoto;
+        private TextView txtLikes;
+
+        public ContactoViewHolder(View itemView) {
+            super(itemView);
+
+            imgFoto     = (ImageView) itemView.findViewById(R.id.imgFoto);
+            txtLikes     = (TextView) itemView.findViewById(R.id.txtLikes);
+
+        }
+    }
+}
